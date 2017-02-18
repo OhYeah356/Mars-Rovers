d57015ddf58255b79a8279565619c608c8acebfc
OhYeah356 <vrzal.ja@gmail.com>
2017-02-18 13:28:48 +0100

    Detail screen UI improvement’s

--- MarsRovers/MarsRovers/AppDelegate.swift
+++ MarsRovers/MarsRovers/AppDelegate.swift
@@ -18,8 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         let rootVC = ViewController()
+        
+        let navigationController = UINavigationController(rootViewController: rootVC)
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = rootVC
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
 
         return true
--- MarsRovers/MarsRovers/ViewController.swift
+++ MarsRovers/MarsRovers/ViewController.swift
@@ -7,37 +7,53 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
-    let button: UIButton = {
-        let button = UIButton(frame: CGRect(x: 20, y: 400, width: 100, height: 100))
+    let leftButton: UIButton = {
+        let button = UIButton()
         button.backgroundColor = .green
-        button.setTitle("Button", for: .normal)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        button.addTarget(self, action: #selector(leftButtonPushed), for: .touchUpInside)
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    let rightButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .green
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        button.addTarget(self, action: #selector(rightButtonPushed), for: .touchUpInside)
+        button.clipsToBounds = true
         return button
     }()
     
     let imageView: UIImageView = {
-        let imageView = UIImageView(frame: CGRect(x: 20, y: 20, width: 250, height: 200))
+        let imageView = UIImageView()
         imageView.backgroundColor = .red
         return imageView
+    }()
+    
+    let cameraNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Front Hazard Avoidance Camera"
+        return label
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = .white
         
-        
-        button.addTarget(self, action: #selector(buttonPushed), for: .touchUpInside)
-        
-        view.addSubview(button)
-        view.addSubview(imageView)
-        
+        setView()
         PhotoModel.shared.getData()
     }
     
-    func buttonPushed(){
+    func leftButtonPushed() {
+        
+    }
+    
+    func rightButtonPushed() {
         let photoData = PhotoModel.shared.data
         
         let url = URL(string: photoData[0].imgURL)
@@ -45,6 +61,45 @@ class ViewController: UIViewController {
         let data = try? Data(contentsOf: url!)
         
         imageView.image = UIImage(data: data!)
+    }
+    
+    func setView() {
+        view.backgroundColor = .white
+        
+        view.addSubview(leftButton)
+        view.addSubview(rightButton)
+        view.addSubview(imageView)
+        view.addSubview(cameraNameLabel)
+        
+        createConstraints()
+    }
+    
+    func createConstraints() {
+        cameraNameLabel.snp.makeConstraints{ make in
+            make.top.equalTo(view).offset(self.navigationController!.navigationBar.frame.height + cameraNameLabel.frame.height + 40)
+            make.centerX.equalTo(view)
+        }
+        
+        imageView.snp.makeConstraints{ make in
+            make.top.equalTo(cameraNameLabel.snp.bottom).offset(20)
+            make.leading.equalTo(view).offset(5)
+            make.trailing.equalTo(view).offset(-5)
+            make.height.equalTo(200)
+        }
+        
+        leftButton.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(40)
+            make.leading.equalTo(10)
+            make.height.equalTo(100)
+            make.width.equalTo(100)
+        }
+        
+        rightButton.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(40)
+            make.trailing.equalTo(-10)
+            make.height.equalTo(100)
+            make.width.equalTo(100)
+        }
     }
 }
 
